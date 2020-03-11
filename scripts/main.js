4852d80c2166fb91b8acafdbfa3f113b0012d270
kierawilson25 <60899868+kierawilson25@users.noreply.github.com>
2020-03-10 23:54:05 -0500

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -3,6 +3,7 @@ var compound_transform;
 // automatically called whenever any transform changes
 function CalculateCompoundTransform(transforms) {
     compound_transform = new Matrix(4, 4); // change / remove this
+    var tranform_matrices = [];
     // matrices in `transforms[i].mat4x4`
     // note `transform[0]` is first tranform to apply to vertex
     
@@ -14,16 +15,12 @@ function CalculateCompoundTransform(transforms) {
     else{
         for(int i =0; i < transforms.length ; i ++)
         {
-            // `compound_transform = Matrix.multiply(...)`
-            compound_transform = Matrix.multiply(transforms[i].mat4x4);
+            transform_matricies.push(transform[i].mat4x4);
+            
         }
+        // `compound_transform = Matrix.multiply(...)`
+        compound_transform = Matrix.multiply(transforms[i].mat4x4);
     }
-        
-    
-    
-    var tranform_matrices = [];
-    
-   
 
     return compound_transform;
 }
@@ -32,8 +29,12 @@ function CalculateCompoundTransform(transforms) {
 function CalculateTransformedVertex(vertex) {
     // multiple vertex by compound_transform
     // `final_vertex = Matrix.multiply(...)`
+    
+    /////Does it really need to be removed
     var final_vertex = new Vector(4); // change / remove this
-
+    //final_vertex = vertex;
+    //Not sure if this stuff is right 
+    final_vertex = Matrix.multiply(vertex);
     return final_vertex;
 }
 
@@ -41,6 +42,7 @@ function CalculateTransformedVertex(vertex) {
 function ChangeTransform(index, type, values) {
     app.transforms[index].type = type;
     // update `app.transforms[index].mat4x4`
+    app.transforms[index].mat4x4;
 
     // recalculate compound transform and tranformed vertex
     app.compound = CalculateCompoundTransform(app.transforms);
